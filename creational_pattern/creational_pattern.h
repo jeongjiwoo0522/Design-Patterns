d5e40c657c7c6028c02e0dc5cb1fd9ac92cdf3ca
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-26 22:58:56 +0900

    CREATE creational_pattern project
    생성 패턴을 익히는데 필요한 프로젝트 생성

---
+++ creational_pattern/creational_pattern.h
@@ -0,0 +1,49 @@
+#pragma once
+
+using namespace std;
+
+enum Direction { North, South, East, West };
+
+class MapSite {
+public:
+	virtual void Enter() = 0;
+};
+
+class Room : public MapSite {
+public:
+	Room(int RoomNo);
+	MapSite* GetSide(Direction, MapSite*);
+	void SetSide(Direction, MapSite*);
+
+	virtual void Enter();
+
+private:
+	MapSite* _sides[4];
+	int _roomNumber;
+};
+
+class Wall : public MapSite {
+public: 
+	Wall();
+	virtual void Enter();
+};
+
+class Door : public MapSite {
+public:
+	Door(Room* = 0, Room* = 0);
+	virtual void Enter();
+	Room* OtherSideFrom(Room*);
+
+private:
+	Room* _room1;
+	Room* _room2;
+	bool _isOpen;
+};
+
+class Maze {
+public:
+	Maze();
+
+	void AddRoom(Room*);
+	Room* RoomNo(int) const;
+};
