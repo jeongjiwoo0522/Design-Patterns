058b67b94bd9a95b5bccacb3e61a11e2765da48c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-27 00:19:43 +0900

    CREATE abstract factory pattern polyfill
    추상 팩토리 패턴의 예제

--- abstract_factory/abstract_factory.h
+++ abstract_factory/abstract_factory.h
@@ -17,3 +17,40 @@ public:
 		return new Door(r1, r2);
 	}
 };
+
+class Spell {};
+class EnchantedRoom : public Room { 
+public: 
+	EnchantedRoom(int n, Spell* spell); 
+};
+class DoorNeedingSpell : public Door {
+public:
+	DoorNeedingSpell(Room*, Room*);
+};
+class EnchantedMazeFactory : public MazeFactory {
+public:
+	EnchantedMazeFactory();
+	virtual Room* MakeRoom(int n) const {
+		return new EnchantedRoom(n, CastSpell());
+	}
+	virtual Door* MakeDoor(Room* r1, Room* r2) const {
+		return new DoorNeedingSpell(r1, r2);
+	}
+protected:
+	Spell* CastSpell() const;
+};
+
+class BombedWall : public Wall {};
+class RoomWithABomb : public Room {
+public:
+	RoomWithABomb(int n);
+};
+class BombedMazeFactory : public MazeFactory {
+public:
+	Wall* MakeWall() const {
+		return new BombedWall();
+	}
+	Room* MakeRoom(int n) const {
+		return new RoomWithABomb(n);
+	}
+};
--- abstract_factory/main.cpp
+++ abstract_factory/main.cpp
@@ -1,3 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
+#include "abstract_factory.h"
+
+class MazeGame {
+public:
+	Maze* CreateMaze(MazeFactory& factory) {
+		Maze* aMaze = factory.MakeMaze();
+		Room* r1 = factory.MakeRoom(1);
+		Room* r2 = factory.MakeRoom(2);
+		Door* aDoor = factory.MakeDoor(r1, r2);
+
+		aMaze->AddRoom(r1);
+		aMaze->AddRoom(r2);
+
+		r1->SetSide(North, factory.MakeWall());
+		r1->SetSide(East, aDoor);
+		r1->SetSide(South, factory.MakeWall());
+		r1->SetSide(West, factory.MakeWall());
+
+		r2->SetSide(North, factory.MakeWall());
+		r2->SetSide(East, factory.MakeWall());
+		r2->SetSide(South, factory.MakeWall());
+		r2->SetSide(West, aDoor);
+	}
+};
+
+int main() {
+	MazeGame game1;
+	MazeGame game2;
+	MazeGame game3;
+
+	MazeFactory basicMazeFactory;
+	BombedMazeFactory bombedMazeFactory;
+	EnchantedMazeFactory enchantedMazeFactory;
+
+	game1.CreateMaze(basicMazeFactory);
+	game2.CreateMaze(bombedMazeFactory);
+	game2.CreateMaze(enchantedMazeFactory);
+
+	return 0;
+}
