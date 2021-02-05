4cd006c7b86ae97484d0b898dc341310fed1ac07
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-05 19:08:48 +0900

    CREATE maze builder

---
+++ builder/builder.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../creational_pattern/creational_pattern.h"
+
+class MazeBuilder {
+public:
+	virtual void BuildMaze() {}
+	virtual void BuildRoom(int room) {} 
+	virtual void BuildDoor(int roomFrom, int roomTo) {}
+
+	virtual Maze* GetMaze() { return 0; }
+
+protected:
+	MazeBuilder();
+};
